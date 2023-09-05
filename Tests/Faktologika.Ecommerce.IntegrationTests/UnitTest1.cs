e90b9d75cbee7d555c02d44b5fa05d35979c048f
shturm <shturm@urghh.net>
2023-09-05 08:08:50 +0300

    add empty integration test proj and sln

---
+++ Tests/Faktologika.Ecommerce.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Faktologika.Ecommerce.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
